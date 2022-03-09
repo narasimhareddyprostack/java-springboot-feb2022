1787775064b861801e3e6088beb81749f8194ee5
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-03-09 11:15:26 +0530

    access

---
+++ Static/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+int id = 101;  //instance var - object
+static String org_Name="TCS"; //static , class level
+public static void main(String[] args){
+	System.out.print(id);	
+}
+}
---
+++ package/User.java
@@ -0,0 +1,7 @@
+package com.prostack.user;
+
+public class User{
+	public static void main(String[] args){
+		System.out.println("Hello, Welcome to Packages");
+	}
+} 
