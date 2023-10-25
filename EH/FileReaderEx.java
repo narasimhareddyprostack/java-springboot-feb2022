8651972e5f451aeb3f53bf0c4f079d49785bf023
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2023-10-25 15:57:23 +0530

    Update FileReaderEx.java

--- EH/FileReaderEx.java
+++ EH/FileReaderEx.java
@@ -1,6 +1,8 @@
 import java.io.FileReader;
 import java.io.*;
 class FileReaderEx{
+
+	
 public static void main(String[] args) throws FileNotFoundException
 {
 	FileReader fr= new FileReader("data.txt");
